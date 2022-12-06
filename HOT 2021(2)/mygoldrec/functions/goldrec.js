0bd382479f60164175f6039b807ea51998cd767f
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2022-12-06 15:34:48 +0800

    Fix mygoldrec

--- HOT 2021(2)/mygoldrec/functions/goldrec.js
+++ HOT 2021(2)/mygoldrec/functions/goldrec.js
@@ -4,56 +4,72 @@
 // Section: 02B
 // Lecturer name: DR. NOORLIN MOHD ALI
 
-function addComma(price) {
+const days = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
 
-    // Remove commas
-    x = price ? parseFloat(price.replace(/,/g, '')) : 0;
+var currentDate = new Date();
+var datetime =
+    currentDate.getFullYear() + "-" +
+    (currentDate.getMonth() + 1) + "-" +
+    currentDate.getDate() + " " +
+    currentDate.getHours() + ":" +
+    currentDate.getMinutes() + ":" +
+    currentDate.getSeconds() +
+    "<br>DAY :- " + days[currentDate.getDay()];
 
-    // Apply formatting
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
-}
+document.getElementById("header_date").innerHTML = datetime;
 
 function checkBalance() {
 
-    var price_1g = document.getElementById("gram1").value;
-    var price_5g = document.getElementById("gram5").value;
-    var price_10g = document.getElementById("gram10").value;
-    var price_20g = document.getElementById("gram20").value;
-    var cap_val = document.getElementById("capital").value;
+    // Get the URL of the current page
+    const url = new URL(window.location.href);
+
+    // Create a new instance of the URLSearchParams interface
+    const params = new URLSearchParams(url.search);
+
+    // Use the get() method of the URLSearchParams interface to retrieve the input value
+    var price_1g = params.get('gram1');
+    var price_5g = params.get('gram5');
+    var price_10g = params.get('gram10');
+    var price_20g = params.get('gram20');
+    var cap_val = parseFloat(params.get('capital'));
 
     document.getElementById("capitalOutput").value = cap_val.toFixed(2);
+    document.getElementById("quantity1").value = 0;
+    document.getElementById("quantity5").value = 0;
+    document.getElementById("quantity10").value = 0;
+    document.getElementById("quantity20").value = 0;
+    document.getElementById("balance").value = cap_val.toFixed(2);
 
     if (cap_val <= 0) {
         return;
     }
 
-    else if (cap_val >= price_20g) {
-        cap_val -= price_20g;
-
-        document.getElementById("balance").value = cap_val;
+    if (cap_val >= price_20g) {
+        var quantity_20g = Math.floor(cap_val / price_20g);
+        document.getElementById("quantity20").value = quantity_20g;
+        cap_val -= price_20g * quantity_20g;
+        document.getElementById("balance").value = cap_val.toFixed(2);
     }
 
-    else if (cap_val >= price_10g) {
-        cap_val -= price_10g;
-
-        document.getElementById("balance").value = cap_val;
+    if (cap_val >= price_10g) {
+        var quantity_10g = Math.floor(cap_val / price_10g);
+        document.getElementById("quantity10").value = quantity_10g;
+        cap_val -= price_10g * quantity_10g;
+        document.getElementById("balance").value = cap_val.toFixed(2);
     }
 
-    else if (cap_val >= price_5g) {
-        cap_val -= price_5g;
-
-        document.getElementById("balance").value = cap_val;
+    if (cap_val >= price_5g) {
+        var quantity_5g = Math.floor(cap_val / price_5g);
+        document.getElementById("quantity5").value = quantity_5g;
+        cap_val -= price_5g * quantity_5g;
+        document.getElementById("balance").value = cap_val.toFixed(2);
     }
 
-    else if (cap_val >= price_1g) {
-        cap_val -= price_1g;
-
-        document.getElementById("balance").value = cap_val;
-    }
-
-    else {
-        return;
+    if (cap_val >= price_1g) {
+        var quantity_1g = Math.floor(cap_val / price_1g);
+        document.getElementById("quantity1").value = quantity_1g;
+        cap_val -= price_1g * quantity_1g;
+        document.getElementById("balance").value = cap_val.toFixed(2);
     }
 
 }

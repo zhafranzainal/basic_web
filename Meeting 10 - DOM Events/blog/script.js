bc4694a4a1d5f34b8d4f821a133675eb2f64d60c
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-08-10 12:41:26 +0800

    Add onclick event for icon up

---
+++ Meeting 10 - DOM Events/blog/script.js
@@ -0,0 +1,9 @@
+function changeText() {
+
+    var replaceText = document.getElementsByClassName("mini-text");
+    replaceText[0].innerHTML = "Scroll down";
+
+    document.getElementId("icon-up").style.display = "none";
+    document.getElementId("icon-down").style.display = "block";
+
+}

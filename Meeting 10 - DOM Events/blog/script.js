0fd49f7caa4bececd2862a921f504f966301c3d7
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-08-10 12:50:43 +0800

    Fix method getElementById

--- Meeting 10 - DOM Events/blog/script.js
+++ Meeting 10 - DOM Events/blog/script.js
@@ -3,7 +3,7 @@ function changeText() {
     var replaceText = document.getElementsByClassName("mini-text");
     replaceText[0].innerHTML = "Scroll down";
 
-    document.getElementId("icon-up").style.display = "none";
-    document.getElementId("icon-down").style.display = "block";
+    document.getElementById("icon-up").style.display = "none";
+    document.getElementById("icon-down").style.display = "block";
 
 }

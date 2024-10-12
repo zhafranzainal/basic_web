7139cd0087236a73df79e627f170f93b2f4a4876
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-10-12 12:21:06 +0800

    Create image slider

--- Meeting 16 - Image Slider using For Each/blog/script.js
+++ Meeting 16 - Image Slider using For Each/blog/script.js
@@ -7,3 +7,29 @@ function changeText() {
     document.getElementById("icon-down").style.display = "block";
 
 }
+
+var slides = document.querySelectorAll(".slide");
+var buttons = document.querySelectorAll(".slider-btn");
+let currentSlide = 1;
+
+//Manual images slider
+var manualNav = function (index) {
+
+    slides.forEach(function (slide) {
+        slide.classList.remove("active");
+        buttons.forEach((btn) => {
+            btn.classList.remove("active");
+        });
+    });
+
+    slides[index].classList.add("active");
+    buttons[index].classList.add("active");
+
+}
+
+buttons.forEach(function (btn, i) {
+    btn.addEventListener("click", function () {
+        manualNav(i);
+        currentSlide = i;
+    });
+})

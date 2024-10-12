082f898dc3804b06da1334b99a034221c46f4f06
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-10-12 11:52:38 +0800

    Create blog

---
+++ Meeting 16 - Image Slider using For Each/blog/script.js
@@ -0,0 +1,9 @@
+function changeText() {
+
+    var replaceText = document.getElementsByClassName("mini-text");
+    replaceText[0].innerHTML = "Scroll down";
+
+    document.getElementById("icon-up").style.display = "none";
+    document.getElementById("icon-down").style.display = "block";
+
+}
